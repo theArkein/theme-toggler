0bf9b9661fa704535d05205e0b22bdf084cff56f
Sarad Singh <theArkein.dev@gmail.com>
2023-05-06 03:05:54 +0545

    Remove external fonts that use SWC

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,5 @@
 import { ThemeProvider } from '@context-providers'
 import './globals.css'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Create Next App',
@@ -16,7 +13,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="dark">
-      <body className={inter.className}>
+      <body>
         <ThemeProvider>
           {children}
         </ThemeProvider>
